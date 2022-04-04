843037b669036dc3d7527ea6c5b07dbcd0333d13
Rajkawale7 <Rajkawale712@gmail.com>
2022-04-04 11:26:43 +0530

    Added: Welcome to the Gambling Game

---
+++ GamblingStart.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class GamblingStart {
+public static void main(String[] args) {
+		
+		System.out.println("*Welcome to the Gambling Game**");
+	
+	}
+}	
+				
+				
